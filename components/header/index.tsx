9eec722877aab102714f49359b8afe8bee21026a
DenserMeerkat <mragulmanoharan@gmail.com>
2024-06-18 20:45:37 +0530

    fix: header z-index

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -6,7 +6,7 @@ import { LoadingSpinner } from "@/components/ui/loading-spinner";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 w-full border-b bg-gradient-to-b from-primary/20 to-primary/10 backdrop-blur-md dark:border-muted dark:from-muted/30 dark:to-muted/10">
+    <header className="sticky top-0 z-50 w-full border-b bg-gradient-to-b from-primary/20 to-primary/10 backdrop-blur-md dark:border-muted dark:from-muted/30 dark:to-muted/10">
       <div className="mx-auto flex max-w-8xl items-center justify-between gap-4 px-4 pb-3 pt-3">
         <Link
           href="/?auth=signin"
